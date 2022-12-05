40fb4b34d603087cba24b73d716002fa3a8a71ca
Alexis Fernando <alexis96fer@gmail.com>
2022-12-04 22:26:56 -0600

    añadiendo Rol al formulario de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
             </a>
         </x-slot>
 
@@ -12,22 +12,40 @@
             <!-- Name -->
             <div>
                 <x-input-label for="name" :value="__('Nombre')" />
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus />
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Type Rol -->
+            <div class="mt-4">
+                <x-input-label for="rol" :value="__('Tipo')" />
+                <select name="rol" id="rol" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                    <option value="1">--Seleccionar Rol--</option>
+                    <option value="2">Developper</option>
+                    <option value="3">DevOps</option>
+                    <option value="4">Data Ciencie</option>
+                    <option value="5">UX/UI</option>
+                    <option value="6">Scrum Master</option>
+                    <option value="7">Front End</option>
+                    <option value="8">Back End</option>
+                    <option value="9">Full Stack</option>
+                    <option value="10">Support</option>
+
+                </select>
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <x-text-input id="password" class="block mt-1 w-full"
+                <x-text-input id="password" class="block w-full mt-1"
                                 type="password"
                                 name="password"
                                 required autocomplete="new-password" />
@@ -39,7 +57,7 @@
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Repetir Password')" />
 
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                <x-text-input id="password_confirmation" class="block w-full mt-1"
                                 type="password"
                                 name="password_confirmation" required />
 
@@ -60,7 +78,7 @@
                 </x-link>
 
             </div>
-            <x-primary-button class="w-full justify-center">
+            <x-primary-button class="justify-center w-full">
                 {{ __('Crear Cuenta') }}
             </x-primary-button>
         </form>
