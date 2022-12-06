d14e53fb8ba04de3e2f8363ddedf82ff8988f812
Alexis Fernando <alexis96fer@gmail.com>
2022-12-06 12:43:17 -0600

    iniciando con el formulario para las vacantes

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,4 +1,29 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
-    <h1>Desde Livewire</h1>
-</div>
+<form action="" class="md:w-1/2" novalidate>
+    {{-- Nombre Vacante --}}
+    <div class="mt-4">
+        <x-input-label for="titulo" :value="__('Vacante')" />
+
+        <x-text-input
+        class="block w-full mt-1"
+        id="titulo"
+        type="text"
+        name="titulo"
+        :value="old('titulo')"
+        placeholder="Nombre de la Vacante"
+        />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+    {{-- Salario --}}
+    <div class="mt-4">
+        <x-input-label for="salario" :value="__('Salario')" />
+
+        <x-text-input
+        class="block w-full mt-1"
+        id="salario"
+        type="number"
+        name="salario"
+        :value="old('salario')"
+        />
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
+    </div>
+</form>
