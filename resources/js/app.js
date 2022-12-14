ad1cfcb722a585b6fbb591919edc3bfb802672d1
Alexis Fernando <alexis96fer@gmail.com>
2022-12-13 19:01:42 -0600

    haciendo pruebas con sweet alert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,30 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+
+import 'sweetalert2/src/sweetalert2.scss';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+Swal.fire({
+    title: '¿Eliminar vacante?',
+    text: "Las vacantes eliminas no se pueden recuperar",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, ¡eliminar!',
+    cancelButtonText: 'Cancelar',
+
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+        )
+    }
+})
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,45 +1,72 @@
-<div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+<div>
+    <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
 
-    @forelse ($vacantes as $vacante )
+        @forelse ($vacantes as $vacante )
 
-    <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
-        <div class="leading-10">
-            <a href="#" class="text-xl font-bold">
-                {{$vacante->titulo}}
-            </a>
-            <p class="text-sm text-gray-600 font-bold">
-                {{$vacante->empresa}}
-            </p>
-            <p class="text-sm text-gray-500 font-normal">
-                Último día para postular: {{ $vacante->fecha_postulacion->format('d/m/Y') }}
-            </p>
+        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+            <div class="leading-10">
+                <a href="#" class="text-xl font-bold">
+                    {{$vacante->titulo}}
+                </a>
+                <p class="text-sm text-gray-600 font-bold">
+                    {{$vacante->empresa}}
+                </p>
+                <p class="text-sm text-gray-500 font-normal">
+                    Último día para postular: {{ $vacante->fecha_postulacion->format('d/m/Y') }}
+                </p>
+            </div>
+
+            <div class="flex flex-col md:flex-row gap-3 items-stretch mt-5 md:mt-0">
+                <a href="#" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold
+                uppercase text-center">
+                Candidatos
+
+                </a>
+                <a href="{{route('vacantes.edit',[$vacante->id])}}" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold
+                uppercase text-center">
+                Editar
+
+                </a>
+                <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold
+                uppercase text-center">
+                Eliminar
+                </a>
+            </div>
         </div>
 
-        <div class="flex flex-col md:flex-row gap-3 items-stretch mt-5 md:mt-0">
-            <a href="#" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold
-            uppercase text-center">
-            Candidatos
+        @empty
 
-            </a>
-            <a href="{{route('vacantes.edit',[$vacante->id])}}" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold
-            uppercase text-center">
-            Editar
+        <p class="p-3 text-center text-sm text-gray-600"> No hay vacantes por mostrar</p>
 
-            </a>
-            <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold
-            uppercase text-center">
-            Eliminar
-            </a>
-        </div>
+        @endforelse
     </div>
 
-    @empty
-
-    <p class="p-3 text-center text-sm text-gray-600"> No hay vacantes por mostrar</p>
-
-    @endforelse
+    <div class=" mt-10">
+        {{$vacantes->links()}}
+    </div>
 </div>
 
-<div class=" mt-10">
-    {{$vacantes->links()}}
-</div>
+
+@push('scripts')
+    <script>
+        Swal.fire({
+            title: '¿Eliminar vacante?',
+            text: "Las vacantes eliminas no se pueden recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, ¡eliminar!',
+            cancelButtonText: 'Cancelar',
+
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+                )
+            }
+        })
+    </script>
+@endpush
