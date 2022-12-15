fdbf22299396cacf5e033483027832db011526c2
Alexis Fernando <alexis96fer@gmail.com>
2022-12-15 15:30:05 -0600

    validando el formulario para postular vacantes

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+
+    protected $rules =[
+        'cv' => 'required|mines:pdf'
+    ];
+
+
+    public function postularme()
+    {
+        $this-> validate();
+
+        // Almacenar el cv
+
+        // Crear la vacante
+
+        // Crear notificacion y enviar email
+
+        // Mostrar msj de ok
+    }
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -2,10 +2,13 @@
     <h3 class="text-center text-2xl font-bold my-4">Postular a la vacante</h3>
 
 
-    <form action="" class="w-96 mt-5">
+    <form action="" class="w-96 mt-5" wire:submit.prevent='postularme'>
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculum')" />
-            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
+            <x-text-input id="cv" type="file" wire:model="cv" accept=".pdf" class="block mt-1 w-full"/>
+
+            <x-input-error :messages="$errors->get('cv')" class="mt-2" />
+
         </div>
 
         <x-primary-button class="my-5">
