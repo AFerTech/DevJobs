fa28112a9f125987a4dfc425cc911470d3e30eff
Alexis Fernando <alexis96fer@gmail.com>
2022-12-17 02:10:15 -0600

    validando y almacenando el cv

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -3,24 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+// Habilitar la carga de archivos
+use Livewire\WithFileUploads;
 class PostularVacante extends Component
 {
+    use WithFileUploads;
+
     public $cv;
 
+
     protected $rules =[
-        'cv' => 'required|mines:pdf'
+        'cv' => 'required|mimes:pdf'
     ];
 
 
     public function postularme()
     {
-        $this-> validate();
+         // si pasa la validacion de $rules, se asigna a datos
+         $datos = $this->validate();
 
-        // Almacenar el cv
-
-        // Crear la vacante
-
+         // Almacenar el cv
+         $cv=$this->cv->store('public/cv');
+         $datos['cv'] = str_replace('public/cv/', '',$cv);
+        // Crear la postulacion
+        // Vacante::create([
+        //     'user_id' => auth()->user()->id,
+        //     ]);
         // Crear notificacion y enviar email
 
         // Mostrar msj de ok
